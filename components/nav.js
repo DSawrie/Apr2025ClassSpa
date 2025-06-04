64c730c71161058a784d2f67eb458bcedbb1f53c
unknown <dylansawrie@gmail.com>
2025-06-03 20:52:41 -0500

    ClassSPA

---
+++ components/nav.js
@@ -0,0 +1,12 @@
+import html from "html-literal";
+
+export default () => html`HTML template literal`;
+    <nav>
+        <l class="fas fa-bars"></l>
+        <ul class="hidden--mobile nav-links">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="aboutMe.html">Bio</a></li>
+            <li><a href="pizza.html">Pizza</a></li>
+            <li><a href="order.html">Order</a></li>
+        </ul>
+    </nav>
